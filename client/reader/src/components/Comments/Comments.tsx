f53d9d12c143a7ecaf1453bd4fd8775cf9abba2d
JunDevl <juninhoplay.pedone@gmail.com>
2026-07-14 22:06:23 -0300

    Improved blog post comment styling

--- client/reader/src/components/Comments/Comments.tsx
+++ client/reader/src/components/Comments/Comments.tsx
@@ -5,6 +5,7 @@ import { createPostComment, fetchPostComments } from "../../actions";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { Suspense, useRef, useState, type SubmitEvent } from "react";
 import { useQueryClient } from "@tanstack/react-query";
+import { format } from "date-fns"; 
 
 type Props = {}
 
@@ -13,9 +14,12 @@ const Comments = (props: Props) => {
 
   const params = useParams();
 
-  const input = useRef<HTMLInputElement>(null);
+  const input = useRef<HTMLTextAreaElement>(null);
+  const buttons = useRef<HTMLDivElement>(null);
+  const commentForm = useRef<HTMLFormElement>(null);
 
   const [isCreatingComment, setIsCreatingComment] = useState(false);
+  const [isFocused, setIsFocused] = useState(false);
 
   const postId = Number(params["postId"]);
 
@@ -33,21 +37,39 @@ const Comments = (props: Props) => {
 
   return (
     <div id="comments">
-      <form id="new_comment" action={async (formData) => {
-        if (isCreatingComment) return;
+      <form 
+        id="new_comment" 
+        action={async (formData) => {
+          if (isCreatingComment) return;
 
-        await createPostComment(formData, postId);
+          await createPostComment(formData, postId);
 
-        await queryClient.fetchQuery({
-          queryKey: ["comments", postId],
-        })
+          await queryClient.fetchQuery({
+            queryKey: ["comments", postId],
+          })
 
-        input.current!.value = "";
+          input.current!.value = "";
 
-        setIsCreatingComment(false);
-      }} onSubmit={submitForm} method="POST">
-        <input type="text" name="content" id="content" ref={input} required/>
-        <button>Comment</button>
+          setIsCreatingComment(false);
+        }} 
+        onSubmit={submitForm} 
+        method="POST"
+        tabIndex={0}
+        onFocus={() => setIsFocused(true)} 
+        onBlur={() => setIsFocused(false)}
+        ref={commentForm}
+      >
+        <textarea 
+          name="content" 
+          id="content" 
+          ref={input}
+          placeholder="Your comment"
+          required
+        />
+        <div className="buttons" ref={buttons}>
+          <button type="reset" className={`${isFocused ? "" : "hidden"}`}>Cancel</button>
+          <button type="submit" className={`accent ${isFocused ? "" : "hidden"}`}>Comment</button>
+        </div>
       </form>
       <Suspense fallback={<p>Loading...</p>}>
         {data.map((comment, i) => {
@@ -58,17 +80,17 @@ const Comments = (props: Props) => {
           return (
             <div className="comment" data-owned={comment.owned} key={i} id={`${i}`}>
               <div className="info">
-                <span className="author">{comment.author.name}</span>
+                <h3 className="author">{comment.author.name}</h3>
                 <span className="created">
                   <time dateTime={createdAt.toUTCString()}>
-                    {createdAt.toLocaleDateString()}
+                    {format(createdAt, "Pp")}
                   </time>
                 </span>
                 {editedAt && 
                   <span className="edited">
                     Edited: 
                     <time dateTime={editedAt.toUTCString()}>
-                      {editedAt.toLocaleDateString()}
+                      {format(editedAt, "Pp")}
                     </time>
                   </span>
                 }
--- client/reader/src/components/ViewingPosts/ViewingPosts.tsx
+++ client/reader/src/components/ViewingPosts/ViewingPosts.tsx
@@ -1,8 +1,11 @@
+import "./viewingpost.css";
+
 import Comments from "../Comments/Comments";
 import { Link, useParams } from "react-router";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { fetchPosts } from "../../actions";
 import { Suspense } from "react";
+import { format } from "date-fns";
 
 type Props = {}
 
@@ -18,24 +21,28 @@ const ViewingPost = (props: Props) => {
 
   const post = posts[postId - 1];
 
+  const createdAt = new Date(post.createdAt);
+
   return (
     <main id="post">
       <Link to="/blog">
         Go back
       </Link>
-      <Suspense fallback={<p>Loading ...</p>}>
-        <h1 className="title">
-          {post.title}
-        </h1>
-        <span className="created">
-          <time dateTime={post.createdAt}>
-            {post.createdAt}
-          </time>
-        </span>
-        <p className="content">
-          {post.content}
-        </p>
-      </Suspense>
+      <div className="blog_post">
+        <Suspense fallback={<p>Loading ...</p>}>
+          <p className="created">
+            <time dateTime={createdAt.toUTCString()}>
+              {format(createdAt, "P")}
+            </time>
+          </p>
+          <h1 className="title">
+            {post.title}
+          </h1>
+          <p className="content">
+            {post.content}
+          </p>
+        </Suspense>
+      </div>
       <Comments/>
     </main>
   )
